8e6958ad3438d295b63ddb7f88ed7f4d995b4527
Maksym Maiboroda <crashdey323@gmail.com>
2021-03-13 15:53:49 +0200

    Start project - create server

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const PORT = 5000;
+
+const app = express();
+
+app.get('/',(req, res)=>{
+    res.status(200).json('Server working!')
+})
+
+app.listen(PORT, ()=> console.log(`SERVER STARTED ON PORT ${PORT}`))
