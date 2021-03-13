9399b1d8d03074964a5197774d84490235b16f50
Maksym Maiboroda <crashdey323@gmail.com>
2021-03-14 00:53:14 +0200

    Create REST API

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-import express from 'express'
+import express    from 'express'
+import mongoose   from 'mongoose'
+import router     from './router.js'
+import fileUpload from 'express-fileupload'
 
-const PORT = 5000;
+const PORT   = 5000;
+const DB_URL = 'mongodb+srv://user:user@cluster0.risin.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 const app = express();
 
-app.get('/',(req, res)=>{
-    res.status(200).json('Server working!')
-})
+app.use(express.json());
+app.use(fileUpload({}));
+app.use('/api', router);
 
-app.listen(PORT, ()=> console.log(`SERVER STARTED ON PORT ${PORT}`))
+
+async function startApp () {
+    try {
+        await mongoose.connect(DB_URL, {useUnifiedTopology : true, useNewUrlParser: true})
+        app.listen(PORT, ()=> console.log(`SERVER STARTED ON PORT ${PORT}`))
+    } catch (e) {
+        console.log(e)
+    }
+};
+
+startApp();
